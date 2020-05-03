270400df941d9f504eee4bebbfb879f731a39731
Farid Jafarlee <farid@bussr.com>
2020-05-03 19:00:42 +0300

    add speed changer input function

--- script.js
+++ script.js
@@ -3,22 +3,32 @@ const pauseButton = document.getElementById("pause-button");
 const stopButton = document.getElementById("stop-button");
 const textInput = document.getElementById("text");
 const speedInput = document.getElementById("speed");
+let currentCharacter;
 
 playButton.addEventListener('click', () => {
   playText(textInput.value)
 });
 pauseButton.addEventListener('click', pauseText);
 stopButton.addEventListener('click', stopText);
+speedInput.addEventListener('input', () => {
+  stopText();
+  playText(utterance.text.substring(currentCharacter));
+});
+
+const utterance = new SpeechSynthesisUtterance(text);
+utterance.addEventListener('end', () => {
+  textInput.disabled = false;
+});
+utterance.addEventListener('boundary', e => {
+  currentCharacter = e.charIndex;
+});
 
 function playText(text) {
   if (speechSynthesis.speaking && speechSynthesis.paused) {
     return speechSynthesis.resume();
   }
-  const utterance = new SpeechSynthesisUtterance(text);
+  utterance.text = text;
   utterance.rate = speedInput.value || 1;
-  utterance.addEventListener('end', () => {
-    textInput.disabled = false;
-  });
   textInput.disabled = true;
   speechSynthesis.speak(utterance);
 }
